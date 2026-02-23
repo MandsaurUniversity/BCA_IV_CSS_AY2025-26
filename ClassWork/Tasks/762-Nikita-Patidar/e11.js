61f72d15ac7e049c96633cd62a25ca71f790c86b
Nikita <patidar.nikita913@gmail.com>
2026-02-23 13:20:32 +0530

    Update e11.js with latest modifications

--- ClassWork/Tasks/762-Nikita-Patidar/e11.js
+++ ClassWork/Tasks/762-Nikita-Patidar/e11.js
@@ -1,14 +1,14 @@
 //Experiment 11:Check Leap Year
-
+//hellogit 
 function isleapyear(year){
     if(year%400==0){
-        console.log("leap year");
+        console.log(year"is leap year");
     }
     else if(year%100==0){
-        console.log("not leap year");
+        console.log(year,"not leap year");
         }
     else if(year%4==0){
-       console.log("leap year");
+       console.log(year,"is leap year");
     }
     else{
         console.log("not a leap year");
