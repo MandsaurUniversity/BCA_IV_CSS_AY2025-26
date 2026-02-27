2e8a96610a527522fc4e707535593f823b9d6204
HARSH <HARSH@AYUSHI>
2026-02-27 11:20:42 +0530

    Better organization of folders

---
+++ ClassWork/Tasks/727-Harsh-Puri/CW/CW3.js
@@ -0,0 +1,19 @@
+//Arrays and It's Fucntions
+const arr=[10,20,30,40,50,60];
+console.log("orignaal array: "+arr);
+arr[2]=10;
+//pushing elemnet 
+arr.push(90);
+console.log("After pushing 90 in array: "+arr);
+//poping from last elemnet POP() 
+arr.pop();
+console.log("After Poping  in array: "+arr);
+//pop elemnet from first shift
+arr.shift();
+console.log("After poping first element in array: "+arr);
+//unshift elemnet 
+arr.unshift(70);
+console.log("After unshift  in array: "+arr);
+//Index of 
+// arr.indexOf(70);
+console.log("indexx of 70  in array: "+arr.indexOf(70));
