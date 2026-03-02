b05f50e570234d86e3178c1a7408493548a2dde7
HARSH <HARSH@AYUSHI>
2026-03-02 13:13:19 +0530

    E12.js program changes

--- ClassWork/Tasks/727-Harsh-Puri/Practice/E12.js
+++ ClassWork/Tasks/727-Harsh-Puri/Practice/E12.js
@@ -1,6 +1,5 @@
 // JS program to Genrate Randwom number between two numbers
-let MIN = 10; let MAX = 20;
 
-let num = Math.floor(Math.random()*(MAX - MIN) +MIN);
+let num =  function (MIN,MAX){   return Math.floor(Math.random()*(MAX - MIN)+MIN)  };
 
-console.log(num);
+console.log(num(10,20));
